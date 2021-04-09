10ca98a9713addedb8e672470a57e6dac5dc8698
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2021-04-09 11:45:00 +0200

    feat: added unsolicited message test
    This test can be used to discover which message events occur when we don't interact
    with a node.

--- src/tests/conformance/messages.rs
+++ src/tests/conformance/messages.rs
@@ -1,12 +1,12 @@
 use crate::{
     protocol::{
-        message::Message,
+        message::{Message, MessageFilter},
         payload::{Nonce, Version},
     },
     setup::{config::read_config_file, node::Node},
 };
 
-use tokio::{io, net::TcpStream};
+use tokio::{io, net::TcpStream, time::timeout, time::Duration};
 
 use std::net::SocketAddr;
 
@@ -50,4 +50,34 @@ async fn handshake(node_addr: SocketAddr) -> io::Result<TcpStream> {
     assert!(matches!(verack, Message::Verack));
 
     Ok(peer_stream)
+}
+
+#[tokio::test]
+#[ignore]
+async fn unsolicitation_listener() {
+    let (_zig, node_meta) = read_config_file();
+
+    let mut node = Node::new(node_meta);
+    node.start().await;
+
+    let mut peer_stream = handshake(node.addr()).await.unwrap();
+
+    let auto_responder = MessageFilter::with_all_auto_reply()
+        .enable_logging();
+
+    for _ in 0usize..10 {
+        let result = timeout(
+            Duration::from_secs(5),
+            auto_responder.read_from_stream(&mut peer_stream),
+        )
+        .await;
+
+        match result {
+            Err(elapsed) => println!("Timeout after {}", elapsed),
+            Ok(Ok(message)) => println!("Received unfiltered message: {:?}", message),
+            Ok(Err(err)) => println!("Error receiving message: {:?}", err),
+        }
+    }
+
+    node.stop().await;
 }
