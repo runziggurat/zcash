185a0b5776785018fad452da164dd099ecaac94a
Niklas <niklas@long.ch>
2021-04-13 12:58:16 +0200

    ref: handshake initiator side

--- src/tests/conformance/handshake.rs
+++ src/tests/conformance/handshake.rs
@@ -10,16 +10,14 @@ use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::test]
 async fn handshake_responder_side() {
-    // 1. Configure and run node.
-    // 2. Send a Version message to the node.
-    // 3. Expect a Version back and send Verack.
-    // 4. Expect Verack back.
+    // ZG-CONFORMANCE-001
 
     let (_zig, node_meta) = read_config_file();
 
     let mut node = Node::new(node_meta);
     node.start().await;
 
+    // Connect to the node and initiate handshake.
     let mut peer_stream = TcpStream::connect(node.addr()).await.unwrap();
 
     Message::Version(Version::new(node.addr(), peer_stream.local_addr().unwrap()))
@@ -43,42 +41,42 @@ async fn handshake_responder_side() {
 
 #[tokio::test]
 async fn handshake_initiator_side() {
+    // ZG-CONFORMANCE-002
+
     let (zig, node_meta) = read_config_file();
 
     let listener = TcpListener::bind(zig.new_local_addr()).await.unwrap();
 
+    // Create a node and set the listener as an initial peer.
     let mut node = Node::new(node_meta);
     node.initial_peers(vec![listener.local_addr().unwrap().port()])
         .start()
         .await;
 
-    match listener.accept().await {
-        Ok((mut peer_stream, addr)) => {
-            let version = Message::read_from_stream(&mut peer_stream).await.unwrap();
-            assert!(matches!(version, Message::Version(..)));
+    // Expect the node to initiate the handshake.
+    let (mut peer_stream, addr) = listener.accept().await.unwrap();
+    let version = Message::read_from_stream(&mut peer_stream).await.unwrap();
+    assert!(matches!(version, Message::Version(..)));
 
-            Message::Version(Version::new(addr, listener.local_addr().unwrap()))
-                .write_to_stream(&mut peer_stream)
-                .await
-                .unwrap();
+    Message::Version(Version::new(addr, listener.local_addr().unwrap()))
+        .write_to_stream(&mut peer_stream)
+        .await
+        .unwrap();
 
-            let verack = Message::read_from_stream(&mut peer_stream).await.unwrap();
-            assert!(matches!(verack, Message::Verack));
+    let verack = Message::read_from_stream(&mut peer_stream).await.unwrap();
+    assert!(matches!(verack, Message::Verack));
 
-            Message::Verack
-                .write_to_stream(&mut peer_stream)
-                .await
-                .unwrap();
-        }
-        Err(e) => println!("couldn't get client: {:?}", e),
-    }
+    Message::Verack
+        .write_to_stream(&mut peer_stream)
+        .await
+        .unwrap();
 
     node.stop().await;
 }
 
 #[tokio::test]
 async fn reject_non_version_replies_to_version() {
-    // Conformance test 004.
+    // ZG-CONFORMANCE-004
     //
     // The node should reject non-Version messages in response to the initial Version it sent.
     //
