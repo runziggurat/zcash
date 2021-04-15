67ebd265c4eb1eaca634baeaf8e65ba1ef6626cb
Niklas <niklas@long.ch>
2021-04-15 22:02:45 +0200

    run rustfmt

--- src/protocol/payload/addr.rs
+++ src/protocol/payload/addr.rs
@@ -15,7 +15,10 @@ pub struct Addr {
 
 impl Addr {
     pub fn empty() -> Self {
-        Self { count: VarInt(0), addrs: Vec::new() }
+        Self {
+            count: VarInt(0),
+            addrs: Vec::new(),
+        }
     }
 
     pub fn encode(&self, buffer: &mut Vec<u8>) -> io::Result<()> {
