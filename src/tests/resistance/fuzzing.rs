b074af819b239df10609d0f8f1aba8c87f009de9
Niklas <niklas@long.ch>
2021-05-06 20:45:18 +0200

    test: version with incorrect length post-handshake

--- src/tests/resistance/fuzzing.rs
+++ src/tests/resistance/fuzzing.rs
@@ -931,6 +931,41 @@ async fn fuzzing_version_with_incorrect_length_during_handshake_responder_side()
 }
 
 #[tokio::test]
+async fn fuzzing_version_with_incorrect_length_post_handshake() {
+    // ZG-RESISTANCE-005 (part 6)
+    //
+    // zebra: disconnects.
+    // zcashd:
+
+    let mut rng = seeded_rng();
+    let (zig, node_meta) = read_config_file();
+
+    let mut node = Node::new(node_meta);
+    node.start_waits_for_connection(zig.new_local_addr())
+        .start()
+        .await;
+
+    for _ in 0..ITERATIONS {
+        let mut peer_stream = initiate_handshake(node.addr()).await.unwrap();
+
+        let version =
+            Message::Version(Version::new(node.addr(), peer_stream.local_addr().unwrap()));
+        let mut message_buffer = vec![];
+        let mut header = version.encode(&mut message_buffer).unwrap();
+
+        // Set the length to a random value which isn't the current value.
+        header.body_length = random_non_valid_u32(&mut rng, header.body_length);
+
+        let _ = header.write_to_stream(&mut peer_stream).await;
+        let _ = peer_stream.write_all(&message_buffer).await;
+
+        autorespond_and_expect_disconnect(&mut peer_stream).await;
+    }
+
+    node.stop().await;
+}
+
+#[tokio::test]
 async fn fuzzing_incorrect_length_during_handshake_responder_side() {
     // ZG-RESISTANCE-002 (part 6)
     //
