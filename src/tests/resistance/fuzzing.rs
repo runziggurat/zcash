5b6a4797afabc4cc0924c5a30e56099420ba3f2b
Niklas <niklas@long.ch>
2021-05-06 20:43:30 +0200

    test: incorrect checksum post-handshake

--- src/tests/resistance/fuzzing.rs
+++ src/tests/resistance/fuzzing.rs
@@ -765,6 +765,56 @@ async fn fuzzing_incorrect_checksum_during_handshake_responder_side() {
 }
 
 #[tokio::test]
+async fn fuzzing_incorrect_checksum_post_handshake() {
+    // ZG-RESISTANCE-005 (part 5)
+    //
+    // zebra:
+    // zcashd:
+
+    let mut rng = seeded_rng();
+    let (zig, node_meta) = read_config_file();
+
+    let mut node = Node::new(node_meta);
+    node.start_waits_for_connection(zig.new_local_addr())
+        .start()
+        .await;
+
+    let test_messages = vec![
+        Message::MemPool,
+        Message::Verack,
+        Message::Ping(Nonce::default()),
+        Message::Pong(Nonce::default()),
+        Message::GetAddr,
+        Message::Addr(Addr::empty()),
+        Message::Headers(Headers::empty()),
+        // Message::GetHeaders(LocatorHashes)),
+        // Message::GetBlocks(LocatorHashes)),
+        // Message::GetData(Inv));
+        // Message::Inv(Inv));
+        // Message::NotFound(Inv));
+    ];
+
+    for _ in 0..ITERATIONS {
+        let message = test_messages.choose(&mut rng).unwrap();
+        let mut message_buffer = vec![];
+        let mut header = message.encode(&mut message_buffer).unwrap();
+
+        // Set the checksum to a random value which isn't the current value.
+        header.checksum = random_non_valid_u32(&mut rng, header.checksum);
+
+        let mut peer_stream = initiate_handshake(node.addr()).await.unwrap();
+
+        // Write messages with wrong checksum.
+        let _ = header.write_to_stream(&mut peer_stream).await;
+        let _ = peer_stream.write_all(&message_buffer).await;
+
+        autorespond_and_expect_disconnect(&mut peer_stream).await;
+    }
+
+    node.stop().await;
+}
+
+#[tokio::test]
 async fn fuzzing_version_with_incorrect_length_pre_handshake() {
     // ZG-RESISTANCE-001 (part 6)
     //
