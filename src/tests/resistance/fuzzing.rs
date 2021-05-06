a14dae7171ed35e865058542bdd3412f551a61a4
Niklas <niklas@long.ch>
2021-05-06 16:03:38 +0200

    test: fuzzing zeroes post-handshake

--- src/tests/resistance/fuzzing.rs
+++ src/tests/resistance/fuzzing.rs
@@ -11,7 +11,7 @@
 // failure or can be inspected with `-- --nocapture`.
 
 use crate::{
-    helpers::{autorespond_and_expect_disconnect, initiate_version_exchange},
+    helpers::{autorespond_and_expect_disconnect, initiate_handshake, initiate_version_exchange},
     protocol::{
         message::*,
         payload::{block::Headers, Addr, Nonce, Version},
@@ -75,6 +75,35 @@ async fn fuzzing_zeroes_during_handshake_responder_side() {
 
     for payload in payloads {
         let mut peer_stream = initiate_version_exchange(node.addr()).await.unwrap();
+
+        // Write zeroes in place of Verack.
+        let _ = peer_stream.write_all(&payload).await;
+
+        autorespond_and_expect_disconnect(&mut peer_stream).await;
+    }
+
+    node.stop().await;
+}
+
+#[tokio::test]
+async fn fuzzing_zeroes_post_handshake() {
+    // ZG-RESISTANCE-005 (part 1)
+    //
+    // zebra: disconnects.
+    // zcashd: responds with ping and getheaders before disconnecting.
+
+    let mut rng = seeded_rng();
+    let payloads = zeroes(&mut rng, ITERATIONS);
+
+    let (zig, node_meta) = read_config_file();
+
+    let mut node = Node::new(node_meta);
+    node.start_waits_for_connection(zig.new_local_addr())
+        .start()
+        .await;
+
+    for payload in payloads {
+        let mut peer_stream = initiate_handshake(node.addr()).await.unwrap();
 
         // Write zeroes in place of Verack.
         let _ = peer_stream.write_all(&payload).await;
