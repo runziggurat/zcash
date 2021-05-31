6474cf93d1bcb1e77d00dc7949700ce842489c37
Niklas <niklas@long.ch>
2021-05-31 16:46:12 +0200

    feat: add inbound channel to synthetic node

--- src/helpers/synthetic_peers.rs
+++ src/helpers/synthetic_peers.rs
@@ -9,24 +9,85 @@ use pea2pea::{
     Connection, Node, Pea2Pea,
 };
 
+use tokio::sync::mpsc::{self, Receiver, Sender};
+
 use std::{
     io::{Cursor, Result},
     net::SocketAddr,
 };
 
-#[derive(Clone)]
-struct SyntheticNode {
-    node: Node,
+pub struct SyntheticNode {
+    inner_node: InnerNode,
+    inbound_rx: Receiver<MessageWithHeader>,
 }
 
-impl Pea2Pea for SyntheticNode {
+impl SyntheticNode {
+    pub fn new(node: Node, enable_handshaking: bool) -> Self {
+        // Inbound channel size of 100 messages.
+        let (tx, rx) = mpsc::channel(100);
+        let inner_node = InnerNode::new(node, tx);
+
+        // Enable the read and write protocols, handshake is enabled on a per-case basis.
+        inner_node.enable_reading();
+        inner_node.enable_writing();
+
+        if enable_handshaking {
+            inner_node.enable_handshaking();
+        }
+
+        Self {
+            inner_node,
+            inbound_rx: rx,
+        }
+    }
+
+    pub async fn connect(&self, target: SocketAddr) -> Result<()> {
+        self.inner_node.node().connect(target).await?;
+
+        Ok(())
+    }
+
+    pub async fn send_direct_message(&self, target: SocketAddr, message: Message) -> Result<()> {
+        let mut payload = vec![];
+        let header = message.encode(&mut payload)?;
+
+        // Encode the header and append the message to it.
+        let mut buffer = Vec::with_capacity(24 + header.body_length as usize);
+        header.encode(&mut buffer)?;
+        buffer.append(&mut payload);
+
+        self.inner_node
+            .node()
+            .send_direct_message(target, buffer.into())
+            .await?;
+
+        Ok(())
+    }
+}
+
+#[derive(Clone)]
+struct InnerNode {
+    node: Node,
+    inbound_tx: Sender<MessageWithHeader>,
+}
+
+impl InnerNode {
+    fn new(node: Node, tx: Sender<MessageWithHeader>) -> Self {
+        Self {
+            node,
+            inbound_tx: tx,
+        }
+    }
+}
+
+impl Pea2Pea for InnerNode {
     fn node(&self) -> &Node {
         &self.node
     }
 }
 
 #[async_trait::async_trait]
-impl Reading for SyntheticNode {
+impl Reading for InnerNode {
     type Message = MessageWithHeader;
 
     fn read_message(
@@ -45,14 +106,19 @@ impl Reading for SyntheticNode {
 
     async fn process_message(&self, source: SocketAddr, message: Self::Message) -> Result<()> {
         // FIXME: implement with message filter.
+
+        if let Err(_) = self.inbound_tx.send(message).await {
+            panic!("synthetic node receiver dropped");
+        };
+
         Ok(())
     }
 }
 
-impl Writing for SyntheticNode {
+impl Writing for InnerNode {
     fn write_message(
         &self,
-        target: SocketAddr,
+        _target: SocketAddr,
         payload: &[u8],
         buffer: &mut [u8],
     ) -> Result<usize> {
@@ -62,7 +128,7 @@ impl Writing for SyntheticNode {
 }
 
 #[async_trait::async_trait]
-impl Handshaking for SyntheticNode {
+impl Handshaking for InnerNode {
     async fn perform_handshake(&self, mut conn: Connection) -> Result<Connection> {
         match !conn.side {
             ConnectionSide::Initiator => {
