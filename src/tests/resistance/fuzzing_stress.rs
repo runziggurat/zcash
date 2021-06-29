de06fdcd74d7aff2c3800a0966fa00c5ca7ebcc3
Mirko von Leipzig <mirkovonleipzig@gmail.com>
2021-06-29 16:54:21 +0200

    ref: move incorrect_length fuzz tests

--- src/tests/resistance/fuzzing_stress.rs
+++ src/tests/resistance/fuzzing_stress.rs
@@ -14,10 +14,12 @@ use crate::{
     },
     setup::node::{Action, Node},
     tests::resistance::{
-        corrupt_message::bad_checksum::encode_messages_and_corrupt_checksum,
+        corrupt_message::{
+            bad_checksum::encode_messages_and_corrupt_checksum,
+            incorrect_length::encode_messages_and_corrupt_body_length_field,
+        },
         default_fuzz_messages,
         fuzzing_corrupted_messages::slightly_corrupted_messages,
-        fuzzing_incorrect_length::encode_messages_and_corrupt_body_length_field,
         fuzzing_random_bytes::{metadata_compliant_random_bytes, random_bytes},
         seeded_rng,
         zeroes::zeroes,
