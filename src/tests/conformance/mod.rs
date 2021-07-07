8b2ccc86877dbe0383e0a66c80d0a674bdc0b4dc
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2021-07-07 15:01:42 +0200

    ref: split ignores_unsolicited_responses

--- src/tests/conformance/mod.rs
+++ src/tests/conformance/mod.rs
@@ -1,3 +1,38 @@
+use std::io;
+
+use crate::{
+    setup::node::{Action, Node},
+    tools::synthetic_node::SyntheticNode,
+};
+
 mod handshake;
 mod invalid_message;
 mod messages;
+mod unsolicited_response;
+
+/// Creates a connected [Node] and [SyntheticNode] which have completed the full handshake.
+///
+/// The [SyntheticNode] initiated the connection.
+///
+/// The [SyntheticNode]'s MessageFilter has been configured for auto replying.
+async fn simple_handshaken_node() -> io::Result<(Node, SyntheticNode)> {
+    // Spin up a node instance.
+    let mut node = Node::new().unwrap();
+    node.initial_action(Action::WaitForConnection)
+        .start()
+        .await
+        .unwrap();
+
+    // Create a synthetic node.
+    let synthetic_node = SyntheticNode::builder()
+        .with_full_handshake()
+        .with_all_auto_reply()
+        .build()
+        .await
+        .unwrap();
+
+    // Connect and initiate the handshake.
+    synthetic_node.connect(node.addr()).await?;
+
+    Ok((node, synthetic_node))
+}
