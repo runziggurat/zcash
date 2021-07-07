c25209e5742ffe053cca7fb68a249dfb0c3fd4b4
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2021-07-07 16:13:26 +0200

    ref: remove simple_handshaken_node
    As discussed in PR #93

--- src/tests/conformance/invalid_message.rs
+++ src/tests/conformance/invalid_message.rs
@@ -17,7 +17,8 @@ use crate::{
         message::Message,
         payload::{block::Block, reject::CCode, FilterAdd, FilterLoad, Inv, Nonce, Version},
     },
-    tests::conformance::simple_handshaken_node,
+    setup::node::{Action, Node},
+    tools::synthetic_node::SyntheticNode,
 };
 
 #[tokio::test]
@@ -98,7 +99,16 @@ async fn bloom_filter_clear() {
 async fn run_test_case(message: Message, expected_code: CCode) -> io::Result<()> {
     const RECV_TIMEOUT: Duration = Duration::from_secs(1);
     // Setup a fully handshaken connection between a node and synthetic node.
-    let (mut node, mut synthetic_node) = simple_handshaken_node().await?;
+    let mut node = Node::new()?;
+    node.initial_action(Action::WaitForConnection)
+        .start()
+        .await?;
+    let mut synthetic_node = SyntheticNode::builder()
+        .with_full_handshake()
+        .with_all_auto_reply()
+        .build()
+        .await?;
+    synthetic_node.connect(node.addr()).await?;
 
     // Send the message to be rejected.
     synthetic_node
--- src/tests/conformance/mod.rs
+++ src/tests/conformance/mod.rs
@@ -1,36 +1,4 @@
-use std::io;
-
-use crate::{
-    setup::node::{Action, Node},
-    tools::synthetic_node::SyntheticNode,
-};
-
 mod handshake;
 mod invalid_message;
 mod messages;
 mod unsolicited_response;
-
-/// Creates a connected [Node] and [SyntheticNode] which have completed the full handshake.
-///
-/// The [SyntheticNode] initiated the connection.
-///
-/// The [SyntheticNode]'s MessageFilter has been configured for auto replying.
-async fn simple_handshaken_node() -> io::Result<(Node, SyntheticNode)> {
-    // Spin up a node instance.
-    let mut node = Node::new()?;
-    node.initial_action(Action::WaitForConnection)
-        .start()
-        .await?;
-
-    // Create a synthetic node.
-    let synthetic_node = SyntheticNode::builder()
-        .with_full_handshake()
-        .with_all_auto_reply()
-        .build()
-        .await?;
-
-    // Connect and initiate the handshake.
-    synthetic_node.connect(node.addr()).await?;
-
-    Ok((node, synthetic_node))
-}
