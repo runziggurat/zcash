3346b8a822c5cb0c1de073a2837efe10dcb6a1ec
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2021-07-08 16:03:43 +0200

    ref: split basic query tests into subcases

--- src/tests/conformance/basic_query.rs
+++ src/tests/conformance/basic_query.rs
@@ -1,3 +1,22 @@
+//! Contains test cases which cover ZG-CONFORMANCE-010
+//!
+//! The node responds with the correct messages. Message correctness is naively verified through successful encoding/decoding.
+//!
+//! This is tested under two different scenarios:
+//!
+//!     1. the node is seeded with the initial testnet blocks.
+//!     2. the node is *not* seeded with data at all.
+//!
+//! Queries and expected replies:
+//!
+//!     - Ping           -> Pong
+//!     - GetAddr        -> Addr
+//!     - Mempool        -> Inv
+//!     - GetBlocks      -> Inv
+//!     - GetHeaders     -> Headers
+//!     - GetData(block) -> Block (1) | NotFound (2)
+//!     - GetData(tx)    -> Tx    (1) | NotFound (2)
+
 use crate::{
     protocol::{
         message::Message,
@@ -7,216 +26,237 @@ use crate::{
         },
     },
     setup::node::{Action, Node},
-    tools::{synthetic_node::SyntheticNode, TIMEOUT},
+    tools::synthetic_node::{PingPongError, SyntheticNode},
 };
-
 use assert_matches::assert_matches;
+use std::{io, time::Duration};
 
-#[tokio::test]
-async fn basic_query_response_seeded() {
-    // ZG-CONFORMANCE-010, node is seeded with data
-    //
-    // The node responds with the correct messages. Message correctness is naively verified through successful encoding/decoding.
-    //
-    // `Ping` expects `Pong`.
-    // `GetAddr` expects `Addr`.
-    // `Mempool` expects `Inv`.
-    // `Getblocks` expects `Inv`.
-    // `GetData(tx_hash)` expects `Tx`.
-    // `GetData(block_hash)` expects `Blocks`.
-    // `GetHeaders` expects `Headers`.
-    //
-    // zebra: DoS `GetData` spam due to auto-response
-    // zcashd: ignores the following messages
-    //             - GetAddr
-    //             - MemPool
-    //
-    //         GetData(tx) returns NotFound (which is correct),
-    //         because we currently can't seed a mempool.
-    //
+const RECV_TIMEOUT: Duration = Duration::from_millis(100);
 
-    let genesis_block = Block::testnet_genesis();
+mod node_is_seeded_with_blocks {
+    use super::*;
 
-    // Spin up a node instance.
-    let mut node = Node::new().unwrap();
-    node.initial_action(Action::SeedWithTestnetBlocks(11))
-        .start()
-        .await
-        .unwrap();
-
-    // Create a synthetic node.
-    let mut synthetic_node = SyntheticNode::builder()
-        .with_full_handshake()
-        .with_all_auto_reply()
-        .build()
-        .await
-        .unwrap();
-
-    // Connect to the node and initiate handshake.
-    synthetic_node.connect(node.addr()).await.unwrap();
-
-    // Ping/Pong.
-    {
-        let ping_nonce = Nonce::default();
-        synthetic_node
-            .send_direct_message(node.addr(), Message::Ping(ping_nonce))
-            .await
-            .unwrap();
-
-        // Verify the nonce matches.
-        let (_, pong) = synthetic_node.recv_message_timeout(TIMEOUT).await.unwrap();
-        assert_matches!(pong, Message::Pong(pong_nonce) if pong_nonce == ping_nonce);
+    #[tokio::test]
+    async fn ping() {
+        // zcashd: pass
+        // zebra:  fail (seeding not supported for zebra)
+        let nonce = Nonce::default();
+        let expected = Message::Pong(nonce);
+        let reply = run_test_case(Message::Ping(nonce)).await.unwrap();
+        assert_eq!(reply, expected);
     }
 
-    // GetAddr/Addr.
-    {
-        synthetic_node
-            .send_direct_message(node.addr(), Message::GetAddr)
-            .await
-            .unwrap();
-
-        let (_, addr) = synthetic_node.recv_message_timeout(TIMEOUT).await.unwrap();
-        assert_matches!(addr, Message::Addr(..));
+    #[tokio::test]
+    async fn get_addr() {
+        // zcashd: fail (query ignored)
+        // zebra:  fail (seeding not supported for zebra)
+        let reply = run_test_case(Message::GetAddr).await.unwrap();
+        assert_matches!(reply, Message::Addr(..));
     }
 
-    // MemPool/Inv.
-    {
-        synthetic_node
-            .send_direct_message(node.addr(), Message::MemPool)
-            .await
-            .unwrap();
-
-        let (_, inv) = synthetic_node.recv_message_timeout(TIMEOUT).await.unwrap();
-        assert_matches!(inv, Message::Inv(..));
+    #[tokio::test]
+    async fn mempool() {
+        // zcashd: fail (query ignored)
+        // zebra:  fail (seeding not supported for zebra)
+        let reply = run_test_case(Message::MemPool).await.unwrap();
+        assert_matches!(reply, Message::Inv(..));
     }
 
-    // GetBlocks/Inv (requesting testnet genesis).
-    {
-        synthetic_node
-            .send_direct_message(
-                node.addr(),
-                Message::GetBlocks(LocatorHashes::new(
-                    vec![genesis_block.double_sha256().unwrap()],
-                    Hash::zeroed(),
-                )),
-            )
-            .await
-            .unwrap();
-
-        let (_, inv) = synthetic_node.recv_message_timeout(TIMEOUT).await.unwrap();
-        assert_matches!(inv, Message::Inv(..));
+    #[tokio::test]
+    async fn get_blocks() {
+        // zcashd: pass
+        // zebra:  fail (seeding not supported for zebra)
+        let query = Message::GetBlocks(LocatorHashes::new(
+            vec![Block::testnet_genesis().double_sha256().unwrap()],
+            Hash::zeroed(),
+        ));
+        let reply = run_test_case(query).await.unwrap();
+        assert_matches!(reply, Message::Inv(..));
     }
 
-    // GetData/Tx.
-    {
-        synthetic_node
-            .send_direct_message(
-                node.addr(),
-                Message::GetData(Inv::new(vec![genesis_block.txs[0].inv_hash()])),
-            )
-            .await
-            .unwrap();
-
-        let (_, tx) = synthetic_node.recv_message_timeout(TIMEOUT).await.unwrap();
-        assert_matches!(tx, Message::Tx(..));
+    #[tokio::test]
+    async fn get_data_block() {
+        // zcashd: pass
+        // zebra:  fail (seeding not supported for zebra)
+        let query = Message::GetData(Inv::new(vec![Block::testnet_2().inv_hash()]));
+        let reply = run_test_case(query).await.unwrap();
+        assert_matches!(reply, Message::Block(..));
     }
 
-    // GetData/Block.
-    {
-        synthetic_node
-            .send_direct_message(
-                node.addr(),
-                Message::GetData(Inv::new(vec![Block::testnet_2().inv_hash()])),
-            )
-            .await
-            .unwrap();
-
-        let (_, block) = synthetic_node.recv_message_timeout(TIMEOUT).await.unwrap();
-        assert_matches!(block, Message::Block(..));
+    #[tokio::test]
+    #[should_panic]
+    // This test should currently fail, since we have no way of seeding the Mempool of the node.
+    async fn get_data_tx() {
+        // zcashd: fail (NotFound), this is expected as we cannot seed the mempool of the node.
+        // zebra:  fail (seeding not supported for zebra)
+        let query = Message::GetData(Inv::new(vec![Block::testnet_genesis().txs[0].inv_hash()]));
+        let reply = run_test_case(query).await.unwrap();
+        assert_matches!(reply, Message::Tx(..));
     }
 
-    // GetHeaders/Headers.
-    {
-        synthetic_node
-            .send_direct_message(
-                node.addr(),
-                Message::GetHeaders(LocatorHashes::new(
-                    vec![genesis_block.double_sha256().unwrap()],
-                    Hash::zeroed(),
-                )),
-            )
-            .await
-            .unwrap();
-
-        let (_, headers) = synthetic_node.recv_message_timeout(TIMEOUT).await.unwrap();
-        assert_matches!(headers, Message::Headers(..));
+    #[tokio::test]
+    async fn get_headers() {
+        // zcashd: pass
+        // zebra:  fail (seeding not supported for zebra)
+        let query = Message::GetHeaders(LocatorHashes::new(
+            vec![Block::testnet_genesis().double_sha256().unwrap()],
+            Hash::zeroed(),
+        ));
+        let reply = run_test_case(query).await.unwrap();
+        assert_matches!(reply, Message::Headers(..));
     }
 
-    // Gracefully shut down the nodes.
-    synthetic_node.shut_down();
-    node.stop().unwrap();
+    async fn run_test_case(query: Message) -> io::Result<Message> {
+        // Spin up a seeded node instance.
+        let mut node = Node::new().unwrap();
+        node.initial_action(Action::SeedWithTestnetBlocks(11))
+            .start()
+            .await?;
+
+        // Create a synthetic node.
+        let mut synthetic_node = SyntheticNode::builder()
+            .with_full_handshake()
+            .with_all_auto_reply()
+            .build()
+            .await?;
+
+        // Connect to the node and initiate handshake.
+        synthetic_node.connect(node.addr()).await?;
+
+        // Send the query.
+        synthetic_node
+            .send_direct_message(node.addr(), query)
+            .await?;
+
+        // Use Ping-Pong to check node's response.
+        let result = match synthetic_node
+            .ping_pong_timeout(node.addr(), RECV_TIMEOUT)
+            .await
+        {
+            Ok(_) => Err(io::Error::new(io::ErrorKind::Other, "Query was ignored")),
+            Err(PingPongError::Unexpected(msg)) => Ok(*msg),
+            Err(err) => Err(err.into()),
+        };
+
+        // Gracefully shut down the nodes.
+        synthetic_node.shut_down();
+        node.stop()?;
+
+        result
+    }
 }
 
-#[tokio::test]
-async fn basic_query_response_unseeded() {
-    // ZG-CONFORMANCE-010, node is *not* seeded with data
-    //
-    // The node responds with the correct messages. Message correctness is naively verified through successful encoding/decoding.
-    //
-    // `GetData(tx_hash)` expects `NotFound`.
-    // `GetData(block_hash)` expects `NotFound`.
-    //
-    // The test currently fails for zcashd and zebra
-    //
-    // Current behaviour:
-    //
-    //  zebra: DDoS spam due to auto-response
-    //  zcashd: Ignores `GetData(block_hash)`
+mod node_is_not_seeded_with_blocks {
+    use super::*;
 
-    // GetData messages...
-    let tx_inv = Inv::new(vec![Block::testnet_genesis().txs[0].inv_hash()]);
-    let block_inv = Inv::new(vec![Block::testnet_2().inv_hash()]);
-
-    let messages = vec![
-        // ...with a tx hash...
-        (tx_inv.clone(), Message::GetData(tx_inv)),
-        // ...and with a block hash.
-        (block_inv.clone(), Message::GetData(block_inv)),
-    ];
-
-    // Spin up a node instance.
-    let mut node = Node::new().unwrap();
-    node.initial_action(Action::WaitForConnection)
-        .start()
-        .await
-        .unwrap();
-
-    // Create a synthetic node with message filtering.
-    let mut synthetic_node = SyntheticNode::builder()
-        .with_full_handshake()
-        .with_all_auto_reply()
-        .build()
-        .await
-        .unwrap();
-
-    // Connect to the node and initiate the handshake.
-    synthetic_node.connect(node.addr()).await.unwrap();
-
-    for (expected_inv, message) in messages {
-        // Send GetData.
-        synthetic_node
-            .send_direct_message(node.addr(), message)
-            .await
-            .unwrap();
-
-        // Assert NotFound is returned.
-        // FIXME: assert on hash?
-        let (_, reply) = synthetic_node.recv_message_timeout(TIMEOUT).await.unwrap();
-        let not_found_inv = assert_matches!(reply, Message::NotFound(inv) => inv);
-        assert_eq!(not_found_inv, expected_inv);
+    #[tokio::test]
+    async fn ping() {
+        // zcashd: pass
+        // zebra:  pass
+        let nonce = Nonce::default();
+        let expected = Message::Pong(nonce);
+        let reply = run_test_case(Message::Ping(nonce)).await.unwrap();
+        assert_eq!(reply, expected);
     }
 
-    // Gracefully shut down the nodes.
-    synthetic_node.shut_down();
-    node.stop().unwrap();
+    #[tokio::test]
+    async fn get_addr() {
+        // zcashd: fail (query ignored)
+        // zebra:  pass
+        let reply = run_test_case(Message::GetAddr).await.unwrap();
+        assert_matches!(reply, Message::Addr(..));
+    }
+
+    #[tokio::test]
+    async fn mempool() {
+        // zcashd: fail (query ignored)
+        // zebra:  fail (query ignored)
+        let reply = run_test_case(Message::MemPool).await.unwrap();
+        assert_matches!(reply, Message::Inv(..));
+    }
+
+    #[tokio::test]
+    async fn get_blocks() {
+        // zcashd: fail (query ignored)
+        // zebra:  fail (query ignored)
+        let query = Message::GetBlocks(LocatorHashes::new(
+            vec![Block::testnet_genesis().double_sha256().unwrap()],
+            Hash::zeroed(),
+        ));
+        let reply = run_test_case(query).await.unwrap();
+        assert_matches!(reply, Message::Inv(..));
+    }
+
+    #[tokio::test]
+    async fn get_data_block() {
+        // zcashd: fail (query ignored)
+        // zebra:  fail (query ignored)
+        let inv = Inv::new(vec![Block::testnet_2().inv_hash()]);
+        let query = Message::GetData(inv.clone());
+        let expected = Message::NotFound(inv);
+        let reply = run_test_case(query).await.unwrap();
+        assert_eq!(reply, expected);
+    }
+
+    #[tokio::test]
+    async fn get_data_tx() {
+        // zcashd: pass
+        // zebra:  fail (query ignored)
+        let inv = Inv::new(vec![Block::testnet_genesis().txs[0].inv_hash()]);
+        let query = Message::GetData(inv.clone());
+        let expected = Message::NotFound(inv);
+        let reply = run_test_case(query).await.unwrap();
+        assert_eq!(reply, expected);
+    }
+
+    #[tokio::test]
+    async fn get_headers() {
+        // zcashd: pass
+        // zebra:  fail (query ignored)
+        let query = Message::GetHeaders(LocatorHashes::new(
+            vec![Block::testnet_genesis().double_sha256().unwrap()],
+            Hash::zeroed(),
+        ));
+        let reply = run_test_case(query).await.unwrap();
+        assert_matches!(reply, Message::Headers(..));
+    }
+
+    async fn run_test_case(query: Message) -> io::Result<Message> {
+        // Spin up a non-seeded node instance.
+        let mut node = Node::new().unwrap();
+        node.initial_action(Action::WaitForConnection)
+            .start()
+            .await?;
+
+        // Create a synthetic node.
+        let mut synthetic_node = SyntheticNode::builder()
+            .with_full_handshake()
+            .with_all_auto_reply()
+            .build()
+            .await?;
+
+        // Connect to the node and initiate handshake.
+        synthetic_node.connect(node.addr()).await?;
+
+        // Send the query.
+        synthetic_node
+            .send_direct_message(node.addr(), query)
+            .await?;
+
+        // Use Ping-Pong to check node's response.
+        let result = match synthetic_node
+            .ping_pong_timeout(node.addr(), RECV_TIMEOUT)
+            .await
+        {
+            Ok(_) => Err(io::Error::new(io::ErrorKind::Other, "Query was ignored")),
+            Err(PingPongError::Unexpected(msg)) => Ok(*msg),
+            Err(err) => Err(err.into()),
+        };
+
+        // Gracefully shut down the nodes.
+        synthetic_node.shut_down();
+        node.stop()?;
+
+        result
+    }
 }
