a3e7e52112ebf3a7238cf0ee855bdb9e407849fd
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2021-07-19 16:41:23 +0200

    feat: added message inplace of verack counter part

--- src/tests/conformance/handshake/ignore_message_inplace_of_verack.rs
+++ src/tests/conformance/handshake/ignore_message_inplace_of_verack.rs
@@ -1,4 +1,4 @@
-//! Contains test cases which cover ZG-CONFORMANCE-005.
+//! Contains test cases which cover ZG-CONFORMANCE-005 and ZG-CONFORMANCE-006.
 //!
 //! The node ignores non-`Verack` message as a response to initial `Verack` it sent.
 
@@ -12,7 +12,7 @@ use crate::{
             Addr, Hash, Inv, Nonce,
         },
     },
-    setup::node::Node,
+    setup::node::{Action, Node},
     tools::synthetic_node::SyntheticNode,
 };
 
@@ -21,8 +21,143 @@ use assert_matches::assert_matches;
 const RECV_TIMEOUT: Duration = Duration::from_millis(100);
 const CONNECTION_TIMEOUT: Duration = Duration::from_secs(10);
 
-mod when_node_initiates_connection {
+mod when_node_receives_connection {
     //! Contains test cases which cover ZG-CONFORMANCE-005.
+
+    use super::*;
+
+    #[tokio::test]
+    async fn get_addr() {
+        // zcashd: pass
+        // zebra:  fail (disconnects)
+        run_test_case(Message::GetAddr).await;
+    }
+
+    #[tokio::test]
+    async fn mempool() {
+        // zcashd: pass
+        // zebra:  fail (disconnects)
+        run_test_case(Message::MemPool).await;
+    }
+
+    #[tokio::test]
+    async fn ping() {
+        // zcashd: pass
+        // zebra:  fail (disconnects)
+        run_test_case(Message::Ping(Nonce::default())).await;
+    }
+
+    #[tokio::test]
+    async fn pong() {
+        // zcashd: pass
+        // zebra:  fail (disconnects)
+        run_test_case(Message::Pong(Nonce::default())).await;
+    }
+
+    #[tokio::test]
+    async fn addr() {
+        // zcashd: pass
+        // zebra:  fail (disconnects)
+        run_test_case(Message::Addr(Addr::empty())).await;
+    }
+
+    #[tokio::test]
+    async fn get_headers() {
+        // zcashd: pass
+        // zebra:  fail (disconnects)
+        let block_hash = Block::testnet_genesis().double_sha256().unwrap();
+        let block_loc = LocatorHashes::new(vec![block_hash], Hash::zeroed());
+        run_test_case(Message::GetHeaders(block_loc)).await;
+    }
+
+    #[tokio::test]
+    async fn get_blocks() {
+        // zcashd: pass
+        // zebra:  fail (disconnects)
+        let block_hash = Block::testnet_genesis().double_sha256().unwrap();
+        let block_loc = LocatorHashes::new(vec![block_hash], Hash::zeroed());
+        run_test_case(Message::GetBlocks(block_loc)).await;
+    }
+
+    #[tokio::test]
+    async fn get_data_block() {
+        // zcashd: pass
+        // zebra:  fail (disconnects)
+        let block_inv = Inv::new(vec![Block::testnet_genesis().inv_hash()]);
+        run_test_case(Message::GetData(block_inv)).await;
+    }
+
+    #[tokio::test]
+    async fn get_data_tx() {
+        // zcashd: pass
+        // zebra:  fail (disconnects)
+        run_test_case(Message::GetData(Inv::new(vec![Block::testnet_genesis()
+            .txs[0]
+            .inv_hash()])))
+        .await;
+    }
+
+    #[tokio::test]
+    async fn inv() {
+        // zcashd: pass
+        // zebra:  fail (disconnects)
+        let block_inv = Inv::new(vec![Block::testnet_genesis().inv_hash()]);
+        run_test_case(Message::Inv(block_inv)).await;
+    }
+
+    #[tokio::test]
+    async fn not_found() {
+        // zcashd: pass
+        // zebra:  fail (disconnects)
+        let block_inv = Inv::new(vec![Block::testnet_genesis().inv_hash()]);
+        run_test_case(Message::NotFound(block_inv)).await;
+    }
+
+    /// Checks that `message` gets ignored when sent instead of [`Message::Version`] when the node
+    /// receives the connection.
+    async fn run_test_case(message: Message) {
+        // Spin up a node instance.
+        let mut node = Node::new().unwrap();
+        node.initial_action(Action::WaitForConnection)
+            .start()
+            .await
+            .unwrap();
+        // Connect to the node, and exchange versions.
+        let mut synthetic_node = SyntheticNode::builder()
+            .with_version_exchange_handshake()
+            .build()
+            .await
+            .unwrap();
+        synthetic_node.connect(node.addr()).await.unwrap();
+
+        // Send a non-verack message.
+        synthetic_node
+            .send_direct_message(node.addr(), message)
+            .await
+            .expect("Sending non-version message");
+
+        // Expect the node to ignore the previous message, verify by completing the handshake.
+        // Send Verack.
+        synthetic_node
+            .send_direct_message(node.addr(), Message::Verack)
+            .await
+            .expect("Sending Verack");
+
+        // Read Verack.
+        let (_, verack) = synthetic_node
+            .recv_message_timeout(RECV_TIMEOUT)
+            .await
+            .expect("Receiving Verack");
+        assert_matches!(verack, Message::Verack);
+
+        // Gracefully shut down the nodes.
+        synthetic_node.shut_down();
+        node.stop().unwrap();
+    }
+}
+
+mod when_node_initiates_connection {
+    //! Contains test cases which cover ZG-CONFORMANCE-006.
     use super::*;
 
     #[tokio::test]
