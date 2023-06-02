b50290c2c7e81b192ceae8622adb1326fde78bdd
Karlo <88337245+Rqnsom@users.noreply.github.com>
2023-06-02 16:07:34 +0200

    feat(synth_node_bin): support for action preconfig
    - It should be possible to configure the action settings before the
      action is even started in some cases.

--- synth_node_bin/src/action/mod.rs
+++ synth_node_bin/src/action/mod.rs
@@ -1,9 +1,10 @@
 use std::net::SocketAddr;
 
 use anyhow::Result;
-use ziggurat_zcash::tools::synthetic_node::SyntheticNode;
+use ziggurat_zcash::tools::{message_filter::MessageFilter, synthetic_node::SyntheticNode};
 
 mod send_get_addr_and_forever_sleep;
+mod advanced_sn_for_s001;
 
 /// Defines properties of any action for a synth node binary.
 ///
@@ -15,6 +16,11 @@ trait SynthNodeAction {
     /// It can be displayed during the runtime.
     fn info(&self) -> &str;
 
+    /// Action configuration.
+    ///
+    /// Allows preconfiguration settings before the action execution starts.
+    fn config(&self) -> ActionCfg;
+
     /// Defines the core action functionality.
     ///
     /// All the program logic happens here.
@@ -22,35 +28,54 @@ trait SynthNodeAction {
 }
 
 /// List of available actions.
+// TODO: Make a command argument to choose action type.
+#[allow(dead_code)]
 pub enum ActionType {
     SendGetAddrAndForeverSleep,
+    AdvancedSnFors001,
+}
+
+/// Action configuration options.
+pub struct ActionCfg {
+    pub msg_filter: MessageFilter,
+}
+
+impl Default for ActionCfg {
+    fn default() -> Self {
+        Self {
+            msg_filter: MessageFilter::with_all_auto_reply(),
+        }
+    }
 }
 
 /// Action handler.
 pub struct ActionHandler {
     /// Internal action.
     action: Box<dyn SynthNodeAction>,
+
+    /// Action startup configuration.
+    pub cfg: ActionCfg,
 }
 
 impl ActionHandler {
     /// Creates a new [`ActionHandler`] for a given [`ActionType`].
     pub fn new(action_type: ActionType) -> Self {
-        Self {
-            action: Box::new(match action_type {
-                ActionType::SendGetAddrAndForeverSleep => {
-                    send_get_addr_and_forever_sleep::Action {}
-                }
-            }),
-        }
+        let action = match action_type {
+            ActionType::SendGetAddrAndForeverSleep => send_get_addr_and_forever_sleep::action(),
+            ActionType::AdvancedSnFors001 => advanced_sn_for_s001::action(),
+        };
+        let cfg = action.config();
+
+        println!(
+            "Running a synth node which performs the following:\n\t{}",
+            action.info()
+        );
+
+        Self { action, cfg }
     }
 
     /// Runs the underlying action.
     pub async fn execute(&self, synth_node: &mut SyntheticNode, addr: SocketAddr) -> Result<()> {
-        println!(
-            "Running a synth node which performs the following:\n\t{}",
-            self.action.info()
-        );
-
         self.action.run(synth_node, addr).await
     }
 }
